31e587dc86d0a49fc7795e5cfdc2c635fef06258
Prathamesh Patil <prathameshpatil2510@gmail.com>
2024-11-07 23:12:39 +0530

    handle input with custom delimiter and negative numbers

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,8 +1,6 @@
-function Add(input: string) {
-
-    const newLineDelimiter = /\n/g;
+function Add(input) {
     const numericValueRegex = /^\d+$/;
-
+    let defaultDelimiter = /,|\n/
     if (input.length === 0) {
         return 0;
     }
@@ -14,8 +12,21 @@ function Add(input: string) {
         }
     }
 
-    const numbers = input.replace(newLineDelimiter, ',').split(',');
-    const sum = numbers.reduce((prevSum, number) => prevSum + parseInt(number, 10), 0);
+    let sum = 0;
+    if (input.substring(0, 2) === "//") {
+        const defaultDelimiterEnd = input.indexOf("\n");
+        defaultDelimiter = new RegExp(input.substring(2, defaultDelimiterEnd));
+        input = input.substring(defaultDelimiterEnd + 1);
+    }
+
+    const numbersArray = input.split(defaultDelimiter).map(Number);
+    const negativeNumbers = numbersArray.filter((number) => number < 0);
+    if(negativeNumbers.length){
+        throw new Error("negative numbers not allowed: " + negativeNumbers.join(", "));
+    }
+    sum = numbersArray.reduce((prevSum, number) => prevSum + parseInt(number), 0);
 
     return sum;
 }
+
+
