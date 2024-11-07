6317052a3bfe3d8f1cc2130402b666745b326669
Prathamesh Patil <prathameshpatil2510@gmail.com>
2024-11-07 22:13:36 +0530

    handle input with \n and ,

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,14 +1,21 @@
-
-
 function Add(input: string) {
+
+    const newLineDelimiter = /\n/g;
+    const numericValueRegex = /^\d+$/;
+
     if (input.length === 0) {
         return 0;
     }
     if (input.length === 1) {
-        if (/^\d+$/.test(input)) {
+        if (numericValueRegex.test(input)) {
             return input;
         } else {
             return 0;
         }
     }
+
+    const numbers = input.replace(newLineDelimiter, ',').split(',');
+    const sum = numbers.reduce((prevSum, number) => prevSum + parseInt(number, 10), 0);
+
+    return sum;
 }
